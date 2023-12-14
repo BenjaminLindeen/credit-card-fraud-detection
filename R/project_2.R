730e7d01cdff4dc0259cd16ffb9d9872b22ce9bf
Benjamin Lindeen <lind1669@umn.edu>
2023-12-14 13:32:29 -0600

    finishing project

--- R/project_2.R
+++ R/project_2.R
@@ -1,38 +1,27 @@
 # Load libraries
 library(caret)
 library(ggplot2)
+library(smotefamily)
 library(rpart)
 library(rpart.plot)
 library(xgboost)
-library(pROC)
-library(dplyr)
+library(ggcorrplot)
 library(pROC)
 
 # Load dataset
 dataset <- read.csv("C:/Users/Benjamin/development/credit-card-fraud-detection/datatset/card_transdata.csv")
 
+# EDA
+summary(dataset)
+head(dataset)
+dim(dataset)
+ggplot(dataset, aes(x = distance_from_home)) + geom_histogram(binwidth = 10, fill = "blue", color = "black")
+ggplot(dataset, aes(x = as.factor(fraud))) +
+  geom_bar(fill = "steelblue") +
+  labs(title = "Class Distribution in Dataset", x = "Fraud", y = "Count")
+
 # Data Manipulation
 dataset <- na.omit(dataset)
-
-# Apply log transformation to skewed variables
-small_constant <- 0.0000001
-dataset <- dataset %>%
-  mutate(distance_from_home_log = log(ifelse(distance_from_home <= 0, small_constant, distance_from_home)),
-         distance_from_last_transaction_log = log(ifelse(distance_from_last_transaction <= 0, small_constant, distance_from_last_transaction)),
-         ratio_to_median_purchase_price_log = log(ifelse(ratio_to_median_purchase_price <= 0, small_constant, ratio_to_median_purchase_price)))
-
-# Create a histogram for distance_from_home_log
-hist(dataset$distance_from_home_log, main = "Histogram of Log Transformed Distance from Home", xlab = "Log Distance from Home")
-
-# Create a histogram for distance_from_last_transaction_log
-hist(dataset$distance_from_last_transaction_log, main = "Histogram of Log Transformed Distance from Last Transaction", xlab = "Log Distance from Last Transaction")
-
-# Create a histogram for ratio_to_median_purchase_price_log
-hist(dataset$ratio_to_median_purchase_price_log, main = "Histogram of Log Transformed Ratio to Median Purchase Price", xlab = "Log Ratio to Median Purchase Price")
-
-# Remove original skewed variables
-dataset <- dataset %>%
-  select(-distance_from_home, -distance_from_last_transaction, -ratio_to_median_purchase_price)
 
 # Split Data
 set.seed(123)
@@ -40,65 +29,40 @@ partition <- createDataPartition(dataset$fraud, p = 0.8, list = FALSE)
 training <- dataset[partition,]
 testing <- dataset[-partition,]
 
-# Calculate class weights
-class_weights <- ifelse(training$fraud == 1, (1 / table(training$fraud)[2]), (1 / table(training$fraud)[1]))
+# Define Features and Labels for SMOTE
+features <- training[, -ncol(training)]  # Exclude the target variable
+labels <- training$fraud                # Target variable
+
+# Count of non-fraudulent transactions
+count_non_fraud <- nrow(training[training$fraud == 0,])
+print(count_non_fraud)
+
+# Count of fraudulent transactions
+count_fraud <- nrow(training[training$fraud == 1,])
+print(count_fraud)
+
+# Calculate dup_size for a 1:1 ratio
+dup_size <- count_non_fraud / count_fraud
+print(dup_size)
+# Apply SMOTE with the calculated dup_size
+smote_data <- SMOTE(features, labels, K = 5, dup_size = 1)
 
 
-# Logistic Regression Model with class weights
-log_model <- glm(fraud ~ ., data = training, family = binomial(), weights = class_weights)
+# Combine the synthetic samples with the original training data
+synthetic_samples1 <- smote_data$data
+synthetic_samples1 <- synthetic_samples1[, !names(synthetic_samples1) %in% "class"]
+synthetic_samples1$fraud <- rep(1, nrow(synthetic_samples1))  # Assign the minority class label
 
+# Add the target variable to the training set for the features
+training_features1 <- training[, -ncol(training)]
+training_features1$fraud <- training$fraud
 
-predictions <- predict(log_model, testing, type = "response")
-predictions <- ifelse(predictions > 0.5, 1, 0)
-confusionMatrix(factor(predictions), factor(testing$fraud))
+# Combine datasets
+training_balanced1 <- rbind(training_features1, synthetic_samples1)
 
-# Linear Regression Model
-lin_model <- lm(fraud ~ ., data = training)
-summary(lin_model)  # This will give you the model summary including coefficients for each variable
+# Check the class distribution
+table(training_balanced1$fraud)
+# Proceed with the rest of the model training and evaluation...
+# (Logistic Regression, Decision Tree, ANN, Gradient Boosting, etc.)
 
-# Predictions from the linear model
-predictions_lin <- predict(lin_model, testing)
-# Thresholding at 0.5 to determine class labels, not recommended for actual classification tasks
-predicted_classes_lin <- ifelse(predictions_lin > 0.5, 1, 0)
-
-# Evaluate predictions
-confusionMatrix(factor(predicted_classes_lin), factor(testing$fraud))
-
-# ... [rest of your code]
-
-# Decision Tree Model using the log-transformed variables
-tree_model <- rpart(fraud ~ ., data = training, method = "class", weights = class_weights)
-predictions <- predict(tree_model, newdata = testing, type = "class")
-predictions_factor <- factor(predictions, levels = c(0, 1))
-testing_fraud_factor <- factor(testing$fraud, levels = c(0, 1))
-confusionMatrix(predictions_factor, testing_fraud_factor)
-rpart.plot(tree_model, type = 3, box.palette = "RdBu", shadow.col = "gray", branch = 1, extra = 102, under = TRUE, cex = 0.6, tweak = 1.2)
-
-# Gradient Boosting Model using the log-transformed variables
-train_data_xgb <- xgb.DMatrix(data = as.matrix(training[, !names(training) %in% "fraud"]), label = training$fraud)
-valid_data_xgb <- xgb.DMatrix(data = as.matrix(testing[, !names(testing) %in% "fraud"]), label = testing$fraud)
-scale_pos_weight <- sum(training$fraud == 0) / sum(training$fraud == 1)
-params <- list(booster = "gbtree", objective = "binary:logistic", eta = 0.1, max_depth = 6, scale_pos_weight = scale_pos_weight)
-watchlist <- list(train = train_data_xgb, eval = valid_data_xgb)
-xgb_model <- xgb.train(params, train_data_xgb, nrounds = 1, watchlist, early_stopping_rounds = 10)
-xgb_predictions <- predict(xgb_model, valid_data_xgb, ntreelimit = xgb_model$best_ntreelimit)
-xgb_predictions <- ifelse(xgb_predictions > 0.5, 1, 0)
-confusionMatrix(factor(xgb_predictions), factor(testing$fraud))
-
-# Additional Metrics
-precision <- posPredValue(factor(xgb_predictions), factor(testing$fraud), positive = "1")
-recall <- sensitivity(factor(xgb_predictions), factor(testing$fraud), positive = "1")
-f1_score <- (2 * precision * recall) / (precision + recall)
-roc_obj <- roc(testing$fraud, as.numeric(xgb_predictions))
-auc <- auc(roc_obj)
-plot(roc_obj, main = "ROC Curve")
-
-# Print metrics
-print(paste("Precision:", precision))
-print(paste("Recall:", recall))
-print(paste("F1 Score:", f1_score))
-print(paste("AUC:", auc))
-
-# Feature Importance for XGBoost
-importance_matrix <- xgb.importance(feature_names = colnames(train_data_xgb), model = xgb_model)
-xgb.plot.importance(importance_matrix)
+# Define Features and Labels for SMOTE
--- R/project_3.R
+++ R/project_3.R
@@ -1,68 +1,103 @@
 # Load libraries
 library(caret)
 library(ggplot2)
-library(smotefamily)
 library(rpart)
 library(rpart.plot)
 library(xgboost)
-library(ggcorrplot)
 library(pROC)
-
-# Load dataset
+library(dplyr)
+library(pROC)
+set.seed(5533)
 dataset <- read.csv("C:/Users/Benjamin/development/credit-card-fraud-detection/datatset/card_transdata.csv")
 
-# EDA
-summary(dataset)
-head(dataset)
-dim(dataset)
-ggplot(dataset, aes(x = distance_from_home)) + geom_histogram(binwidth = 10, fill = "blue", color = "black")
-ggplot(dataset, aes(x = as.factor(fraud))) +
-  geom_bar(fill = "steelblue") +
-  labs(title = "Class Distribution in Dataset", x = "Fraud", y = "Count")
+# Apply log transformation to skewed variables
+dataset <- na.omit(dataset) # Data Manipulation
+small_constant <- 0.0000001
+dataset <- dataset %>%
+  mutate(distance_from_home_log = log(ifelse(distance_from_home <= 0, small_constant, distance_from_home)),
+         distance_from_last_transaction_log = log(ifelse(distance_from_last_transaction <= 0, small_constant, distance_from_last_transaction)),
+         ratio_to_median_purchase_price_log = log(ifelse(ratio_to_median_purchase_price <= 0, small_constant, ratio_to_median_purchase_price)))
 
-# Data Manipulation
-dataset <- na.omit(dataset)
+# Create a histogram for distance_from_home_log
+hist(dataset$distance_from_home_log, main = "Histogram of Log Transformed Distance from Home", xlab = "Log Distance from Home")
+
+# Create a histogram for distance_from_last_transaction_log
+hist(dataset$distance_from_last_transaction_log, main = "Histogram of Log Transformed Distance from Last Transaction", xlab = "Log Distance from Last Transaction")
+
+# Create a histogram for ratio_to_median_purchase_price_log
+hist(dataset$ratio_to_median_purchase_price_log, main = "Histogram of Log Transformed Ratio to Median Purchase Price", xlab = "Log Ratio to Median Purchase Price")
+
+# Remove original skewed variables
+dataset <- dataset %>%
+  select(-distance_from_home, -distance_from_last_transaction, -ratio_to_median_purchase_price)
 
 # Split Data
-set.seed(123)
 partition <- createDataPartition(dataset$fraud, p = 0.8, list = FALSE)
 training <- dataset[partition,]
 testing <- dataset[-partition,]
 
-# Define Features and Labels for SMOTE
-features <- training[, -ncol(training)]  # Exclude the target variable
-labels <- training$fraud                # Target variable
+# Calculate class weights
+class_weights <- ifelse(training$fraud == 1, (1 / table(training$fraud)[2]), (1 / table(training$fraud)[1]))
 
-# Count of non-fraudulent transactions
-count_non_fraud <- nrow(training[training$fraud == 0,])
-print(count_non_fraud)
+# Linear Regression Model
+lin_model <- lm(fraud ~ ., data = training)
+summary(lin_model)  # This will give you the model summary including coefficients for each variable
+predictions_lin <- predict(lin_model, testing) # Predictions from the linear model
+predicted_classes_lin <- ifelse(predictions_lin > 0.5, 1, 0) # Thresholding at 0.5 to determine class labels, not recommended for actual classification tasks
+confusionMatrix(factor(predicted_classes_lin), factor(testing$fraud))
 
-# Count of fraudulent transactions
-count_fraud <- nrow(training[training$fraud == 1,])
-print(count_fraud)
+# Existing confusion matrix calculation
+conf_matrix_lin <- confusionMatrix(factor(predicted_classes_lin), factor(testing$fraud))
 
-# Calculate dup_size for a 1:1 ratio
-dup_size <- count_non_fraud/ count_fraud
-print(dup_size)
-# Apply SMOTE with the calculated dup_size
-smote_data <- SMOTE(features, labels, K = 5, dup_size = 1)
+# Calculate precision, recall, and F1 score
+precision_lin <- conf_matrix_lin$byClass['Pos Pred Value']
+recall_lin <- conf_matrix_lin$byClass['Sensitivity']
+f1_score_lin <- 2 * precision_lin * recall_lin / (precision_lin + recall_lin)
 
+# Print metrics
+print(paste("Linear Regression - Precision:", precision_lin))
+print(paste("Linear Regression - Recall:", recall_lin))
+print(paste("Linear Regression - F1 Score:", f1_score_lin))
 
-# Combine the synthetic samples with the original training data
-synthetic_samples1 <- smote_data$data
-synthetic_samples1 <- synthetic_samples1[, !names(synthetic_samples1) %in% "class"]
-synthetic_samples1$fraud <- rep(1, nrow(synthetic_samples1))  # Assign the minority class label
+# Logistic Regression Model with class weights
+log_model <- glm(fraud ~ ., data = training, family = binomial(), weights = class_weights)
+predictions <- predict(log_model, testing, type = "response")
+predictions <- ifelse(predictions > 0.5, 1, 0)
+confusionMatrix(factor(predictions), factor(testing$fraud))
 
-# Add the target variable to the training set for the features
-training_features1 <- training[, -ncol(training)]
-training_features1$fraud <- training$fraud
+# Existing confusion matrix calculation
+conf_matrix_log <- confusionMatrix(factor(predictions), factor(testing$fraud))
 
-# Combine datasets
-training_balanced1 <- rbind(training_features1, synthetic_samples1)
+# Calculate precision, recall, and F1 score
+precision_log <- conf_matrix_log$byClass['Pos Pred Value']
+recall_log <- conf_matrix_log$byClass['Sensitivity']
+f1_score_log <- 2 * precision_log * recall_log / (precision_log + recall_log)
 
-# Check the class distribution
-table(training_balanced1$fraud)
-# Proceed with the rest of the model training and evaluation...
-# (Logistic Regression, Decision Tree, ANN, Gradient Boosting, etc.)
+# Print metrics
+print(paste("Logistic Regression - Precision:", precision_log))
+print(paste("Logistic Regression - Recall:", recall_log))
+print(paste("Logistic Regression - F1 Score:", f1_score_log))
 
-# Define Features and Labels for SMOTE
+# Evaluate predictions
+confusionMatrix(factor(predicted_classes_lin), factor(testing$fraud))
+
+# Decision Tree Model using the log-transformed variables
+tree_model <- rpart(fraud ~ ., data = training, method = "class", weights = class_weights)
+predictions <- predict(tree_model, newdata = testing, type = "class")
+predictions_factor <- factor(predictions, levels = c(0, 1))
+testing_fraud_factor <- factor(testing$fraud, levels = c(0, 1))
+rpart.plot(tree_model, type = 3, box.palette = "RdBu", shadow.col = "gray", branch = 1, extra = 102, under = TRUE, cex = 0.6, tweak = 1.2)
+confusionMatrix(predictions_factor, testing_fraud_factor)
+
+# Existing confusion matrix calculation
+conf_matrix_tree <- confusionMatrix(predictions_factor, testing_fraud_factor)
+
+# Calculate precision, recall, and F1 score
+precision_tree <- conf_matrix_tree$byClass['Pos Pred Value']
+recall_tree <- conf_matrix_tree$byClass['Sensitivity']
+f1_score_tree <- 2 * precision_tree * recall_tree / (precision_tree + recall_tree)
+
+# Print metrics
+print(paste("Decision Tree - Precision:", precision_tree))
+print(paste("Decision Tree - Recall:", recall_tree))
+print(paste("Decision Tree - F1 Score:", f1_score_tree))
